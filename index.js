2d01726e69f0d2add5cf4c989ca25a7db4f137fc
gtpWebDev <33732289+gtpWebDev@users.noreply.github.com>
2024-02-27 17:02:00 +0000

    Internalise gameboard to game function
    Added the gameboard as a parameter of the game factory function
    to limit globally scoped code to the game only.

--- index.js
+++ index.js
@@ -1,96 +1,70 @@
 
-
-
-
-
-
-
-
-// Invokes a single instance of a gameboard object immediately
-// contains in-built functionality of reporting on all positions on game board, updating and resetting gameboard, and identifying game status - has anybody won yet, etc.
+// Immediately invoked function expression (IIFE) - invokes a single instance of a gameboard object with functionality to get positions on the board, and to reset and update the board.
 const gameboard = (function() {
 
-  function checkMatch(value, pos1, pos2, pos3) {
-    if (state[pos1] === value && state[pos2] === value && state[pos3] === value) {
-      return true
-    } else {
-      return false
-    }
-  }
+  const boardArray = []
 
-  const state = []
+  const getPosition = (position) => boardArray[position]
+
+  // internal function
+  // function checkMatch(value, pos1, pos2, pos3) {
+  //   if (boardArray[pos1] === value && boardArray[pos2] === value && boardArray[pos3] === value) {
+  //     return true
+  //   } else {
+  //     return false
+  //   }
+  // }
 
   const reset = () => {
     for (let i = 0; i < 9; i++) {
-      state[i] = ""
+      boardArray[i] = ""
     }
   }
 
-  const update = ( position, updateTo ) => { state[position] = updateTo }
+  const updatePosition = ( position, updateTo ) => { boardArray[position] = updateTo }
 
-  
-  const gameStatus = () => {
-    // 1. any win condition met - "xWin" or "oWin"
-    // 2. win condition not met but board full - "tie"
-    // 3. win condition not met and tie condition not met - "ongoing"
-    if ( checkMatch("X",0,1,2) || checkMatch("X",3,4,5) || checkMatch("X",6,7,8) || checkMatch("X",0,3,6) ||
-         checkMatch("X",1,4,7) || checkMatch("X",2,5,8) || checkMatch("X",0,4,8) || checkMatch("X",2,4,6)
-    ) {
-      return "xWin";
-    } else if ( checkMatch("O",0,1,2) || checkMatch("O",3,4,5) || checkMatch("O",6,7,8) || checkMatch("O",0,3,6) ||
-                checkMatch("O",1,4,7) || checkMatch("O",2,5,8) || checkMatch("O",0,4,8) || checkMatch("O",2,4,6)
-    ) {
-      return "oWin"
-    } else if (!state.includes("")) {
-      return "tie"
-    } else {
-      return "ongoing"
-    }
-  }
+  // const gameStatus = () => {
+  //   if ( checkMatch("X",0,1,2) || checkMatch("X",3,4,5) || checkMatch("X",6,7,8) || checkMatch("X",0,3,6) ||
+  //        checkMatch("X",1,4,7) || checkMatch("X",2,5,8) || checkMatch("X",0,4,8) || checkMatch("X",2,4,6)
+  //   ) {
+  //     return "xWin";
+  //   } else if ( checkMatch("O",0,1,2) || checkMatch("O",3,4,5) || checkMatch("O",6,7,8) || checkMatch("O",0,3,6) ||
+  //               checkMatch("O",1,4,7) || checkMatch("O",2,5,8) || checkMatch("O",0,4,8) || checkMatch("O",2,4,6)
+  //   ) {
+  //     return "oWin"
+  //   } else if (!boardArray.includes("")) {
+  //     return "tie"
+  //   } else {
+  //     return "ongoing"
+  //   }
+  // }
 
-  return { state, reset, update, gameStatus }
+  const getFullBoard = () => boardArray
+
+  return { reset, getPosition, updatePosition, getFullBoard }
 
 })()
 
 
-
-
-// Factory function to set up people, who will be a pool of people that are eligible to play the game
+// Factory function to create "person" objects, who will be eligible to play games
 const createPerson = function (name) {
 
-  let nickname = ""
+  // private variables, intentionally only accessible through factory function functions
+  let nickname = "bum"
   let totalWins = 0
 
   const addNickname = (newName) => { nickname = newName }
   const getNickname = () => nickname
-  const addWin = () => totalWins++
-  const getWins = () => totalWins
 
-  return { name, getNickname, addNickname, addWin, getWins }
+  const getWins = () => totalWins
+  const addWin = () => totalWins++
+  
+  return { name, getNickname, addNickname, getWins, addWin }
 
 }
 
 
-// GAMELOGIC
-
-/*
-  STEP 1: COLLECT 2 PLAYERS
-
-  STEP 2: ASSIGN X AND O TO PLAYERS. X ALWAYS STARTS
-
-  STEP N: GAME TURN:
-    - DETERMINE ACTIVE PLAYER
-    - USER SELECTS BOARD POSITION
-    - GAME BOARD UPDATED FOR SELECTION
-    - CHECK WIN CONDITIONS - DIRECT TO WIN OR ANOTHER GAME TURN
-
-  END GAME WHEN WON
-
-  STEP 3: WIN - COMMUNICATE IT, ETC.
-
-*/
-
-const createGame = function(player1, player2) {
+const createGame = function(gameboard, player1, player2) {
   
   let xPlayer
   let oPlayer
@@ -99,6 +73,14 @@ const createGame = function(player1, player2) {
   let gameWinner = "None"
   let gameActive = false // whether game is active or not
 
+  // internal function
+  function checkMatch(value, pos1, pos2, pos3) {
+    if (gameboard.getPosition(pos1) === value && gameboard.getPosition(pos2) === value && gameboard.getPosition(pos3) === value) {
+      return true
+    } else {
+      return false
+    }
+  }
 
   const initialiseGame = () => {
     if (Math.random()<=0.5) {
@@ -111,7 +93,29 @@ const createGame = function(player1, player2) {
     activePlayer = xPlayer;
     activeMark = "X";
     gameActive = true;
+    gameboard.reset()
   }
+
+  const checkPlayerSelection = (chosenPosition) => gameboard.getPosition(chosenPosition) === "" ? true : false
+
+  const addPlayerSelection = (boardPosition, activeMark) => gameboard.updatePosition(boardPosition,activeMark)
+
+  const checkGameStatus = () => {
+    if ( checkMatch("X",0,1,2) || checkMatch("X",3,4,5) || checkMatch("X",6,7,8) || checkMatch("X",0,3,6) ||
+         checkMatch("X",1,4,7) || checkMatch("X",2,5,8) || checkMatch("X",0,4,8) || checkMatch("X",2,4,6)
+    ) {
+      return "xWin";
+    } else if ( checkMatch("O",0,1,2) || checkMatch("O",3,4,5) || checkMatch("O",6,7,8) || checkMatch("O",0,3,6) ||
+                checkMatch("O",1,4,7) || checkMatch("O",2,5,8) || checkMatch("O",0,4,8) || checkMatch("O",2,4,6)
+    ) {
+      return "oWin"
+    } else if (!gameboard.getFullBoard().includes("")) {
+      return "tie"
+    } else {
+      return "ongoing"
+    }
+  }
+
 
   const swapPlayer = () => {
     if (activeMark === "X") {
@@ -127,15 +131,12 @@ const createGame = function(player1, player2) {
     switch (gameResult) {
       case "xWin":
         gameWinner = xPlayer;
-        console.log("xWin, gameWinner assigned `xPlayer`");
         break;
       case "oWin":
         gameWinner = oPlayer;
-        console.log("oWin, gameWinner assigned `oPlayer`");
         break;
       case "tie":
         gameWinner = "None";
-        console.log(`gameWinner should be "none": ${gameWinner}`);
         break;
     }
     gameActive = false;
@@ -148,7 +149,7 @@ const createGame = function(player1, player2) {
   const getGameActive = () => gameActive;
   const getGameWinner = () => gameWinner;
   
-  return { player1, player2, initialiseGame, getXPlayer, getOPlayer, getActiveMark, getActivePlayer, swapPlayer, getGameActive, endGame, getGameWinner }
+  return { player1, player2, initialiseGame, getXPlayer, getOPlayer, getActiveMark, getActivePlayer, checkPlayerSelection, addPlayerSelection, checkGameStatus, swapPlayer, getGameActive, endGame, getGameWinner }
 
 }
 
@@ -163,63 +164,55 @@ console.log(`${glen.name}, otherwise known as ${glen.getNickname()}, is eligible
 const joe = createPerson("Joe"); joe.addNickname("Old Hopeless")
 console.log(`${joe.name}, otherwise known as ${joe.getNickname()}, is eligible to play, and has won ${joe.getWins()} games.`)
 
+
 // stage 1 - GAME PREPARATION - create the game with two players, reset game board, allocate X and O to the two players. (X always goes first).
+let activeGame = createGame(gameboard, glen, joe)
+activeGame.initialiseGame()
+console.log(`Game has been initialised. ${activeGame.getXPlayer().name} will play X. ${activeGame.getOPlayer().name} will play O. ${activeGame.getActivePlayer().name} will start.`)
 
-for (let i = 0; i<10; i++) {
+// stage 2 - REPEATED GAME TURN:
+while ( activeGame.getGameActive() ) {
 
-    let activeGame = createGame(glen,joe)
-    activeGame.initialiseGame()
-    gameboard.reset()
-    console.log(`Game has been initialised. ${activeGame.getXPlayer().name} will play X. ${activeGame.getOPlayer().name} will play O. ${activeGame.getActivePlayer().name} will start.`)
+  // CHECK PLAYER SELECTION
+  let chosenPosition
+  do {
+    chosenPosition = Math.floor(Math.random()*8.99)
+  } while ( !activeGame.checkPlayerSelection(chosenPosition) )
 
-    // stage 2 - GAME TURN:
-    while ( activeGame.getGameActive() ) {
-      // stage 2a - receive position from active player, check valid, repeat until valid
+  // PROCESS PLAYER SELECTION
+  activeGame.addPlayerSelection(chosenPosition,activeGame.getActiveMark())
 
-      let validChoice = false
-      let chosenPosition
-      while ( validChoice === false ) {
-        chosenPosition = Math.floor(Math.random()*8.99)
-        
-        if (gameboard.state[chosenPosition] === "") {
-          validChoice = true
-        }
-      }
+  //gameboard.updatePosition(chosenPosition,activeGame.getActiveMark())
+  //console.log(`${activeGame.getActiveMark()} takes position ${chosenPosition}`)
+  //console.table(gameboard.state)
+  //console.log(`Current game status is ${gameboard.gameStatus()}`)
 
-      // stage 2b - update board with choice, check board game status for win / tie / ongoing
 
-      
-      gameboard.update(chosenPosition,activeGame.getActiveMark())
-      //console.log(`${activeGame.getActiveMark()} takes position ${chosenPosition}`)
-      //console.table(gameboard.state)
-      //console.log(`Current game status is ${gameboard.gameStatus()}`)
 
-      // stage 2c - check whether game over or not
-      if (gameboard.gameStatus() === "xWin" || gameboard.gameStatus() === "oWin" || gameboard.gameStatus() === "tie") {
+  // stage 2c - check whether game over or not
+  if (activeGame.checkGameStatus() === "xWin" || activeGame.checkGameStatus() === "oWin" || activeGame.checkGameStatus() === "tie") {
 
-        // if game over, end game
-        activeGame.endGame(gameboard.gameStatus())  
+    // if game over, end game
+    activeGame.endGame(activeGame.checkGameStatus())  
 
-      } else {
+  } else {
 
-        // if game not over, swap player and continue
-        activeGame.swapPlayer()
-
-      }
-
-    }
-
-    // stage 3 - communicate game result and add wins to player if appropriate
-    
-    console.log(`Game result is ${gameboard.gameStatus()}`)
-
-    if (gameboard.gameStatus() === "xWin" || gameboard.gameStatus() === "oWin") {
-      let gameWinner = activeGame.getGameWinner()
-      gameWinner.addWin()
-      console.log(`Win added for ${activeGame.getGameWinner().name}`)
-    }
+    // if game not over, swap player and continue
+    activeGame.swapPlayer()
 
   }
 
-  console.log(`Final score for 10 rounds is Glen: ${glen.getWins()}, Joe: ${joe.getWins()}`)
-      
+    // PROCESS ROUND END
+
+}
+
+// stage 3 - communicate game result and add wins to player if appropriate
+
+console.log(`Game result is ${activeGame.checkGameStatus()}`)
+
+if (activeGame.checkGameStatus() === "xWin" || activeGame.checkGameStatus() === "oWin") {
+  let gameWinner = activeGame.getGameWinner()
+  gameWinner.addWin()
+  console.log(`Win added for ${activeGame.getGameWinner().name}`)
+}
+
